e954efd8e9920ef4ba36c3d25199557aac0b40ff
Mark Linn <marktlinn@gmail.com>
2022-09-01 17:48:20 +0200

    mongoose schema for todos + Model set up, test in postman and working

---
+++ backend/models/todosModels.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const todo = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    toFinishBy:{
+        type: Date,
+        required: false
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    completed:{
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = mongoose.model('Todo', todo);
--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,20 +1,26 @@
 const express = require('express');
-
+const Todo = require('../models/todosModels')
 const router = express.Router()
 
 router.get('/', (req,res)=>{
     res.send(`<h1>Todos Live Here!</h1>`)
 })
 
-//Gets a singular todo
+//Gets a singular Todo
 router.get('/:id', (req,res)=>{
     res.send(`<h1>Just one ToDo!</h1>`)
 })
 
 
 //Post a new Todo
-router.post('/', (req,res)=>{
-    res.send(`<h1>Post a new ToDo...</h1>`)
+router.post('/', async (req,res)=>{
+    const { title, description} = req.body;
+    try {
+        const todo = await Todo.create({title, description})
+        res.status(200).json(todo);
+    } catch (error) {
+        res.status(500).json(`Error: ${error.message}`)
+    }
 })
 
 // Delete a Todo
