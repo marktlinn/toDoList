3d75cb45fc5a2205c6e216f46021f8558efb76f2
Mark Linn <marktlinn@gmail.com>
2022-09-05 15:23:59 +0200

    deletes and gets working fully now, removed some code from index.html which logged errors

--- backend/controllers/todosController.js
+++ backend/controllers/todosController.js
@@ -28,7 +28,7 @@ const getTodo = async (req,res)=>{
         }
         res.status(200).json(todo)
     } catch (error) {
-        res.status(500).json({Error: error.message})
+        res.status(500).json({error: error.message})
     }
 }
 
@@ -45,15 +45,19 @@ const createTodo = async (req,res)=>{
 // delete todo
 const deleteTodo = async (req,res)=>{
     const { id } = req.params;
+    try {
         if(!mongoose.Types.ObjectId.isValid(id)){
             return res.status(404).json({error: 'No such todo exists'})
         }
         const todo = await Todo.findOneAndDelete({_id: id})
-
         if(!todo){
             return res.status(400).json({error: 'no such todo found to delete'})
-        }    
-        res.status(200).json(todo);
+        }   
+        res.status(200).json(todo);    
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+
 }
 // update todo
 const updateTodo = async (req,res)=>{
