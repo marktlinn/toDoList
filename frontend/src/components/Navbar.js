928393bd5b046dd8e1ca1c67d498ed03f42b735a
Mark Linn <marktlinn@gmail.com>
2022-09-07 16:59:12 +0200

    new branch set up to change navbar for mobile

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const Navbar = () => {
+    //track the wondow
+    const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+    useEffect(()=>{
+        const resizeScreen = () =>{
+            setWindowSize(window.innerWidth)
+        }
+        window.addEventListener('resize', resizeScreen)
+
+        return () => {window.removeEventListener('resize', resizeScreen)}
+    }, [])
+    console.log(windowSize)
+
     return (
         <header>
             <div className="container">
