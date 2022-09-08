c2f6755d14fa63affbe3b5cf4a84edf36fddf0f6
Mark Linn <marktlinn@gmail.com>
2022-09-08 18:12:37 +0200

    updated styling for sideNav bar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,19 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
+
+//Font Awesome styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 const Navbar = () => {
     //track the wondow
     const [windowSize, setWindowSize] = useState(window.innerWidth);
 
     useEffect(()=>{
         const resizeScreen = () =>{
-            setWindowSize(window.innerWidth)
+            return setWindowSize(window.innerWidth)
         }
         window.addEventListener('resize', resizeScreen)
 
         return () => {window.removeEventListener('resize', resizeScreen)}
     }, [])
-    console.log(windowSize)
+
 
     return (
         <header>
@@ -22,9 +26,18 @@ const Navbar = () => {
             <Link to="/">
                 <h1>ToDos</h1>
             </Link>
+            {windowSize <= '600' && <FontAwesomeIcon 
+            className='faBars' 
+            icon={faBars} 
+            />}
             </div>
         </header>
     )
 }
 
 export default Navbar
+
+/*
+If screen more than 600, don't show menu.
+If screen less than 600, form is a toggle menu
+*/
