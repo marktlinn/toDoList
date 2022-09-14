4d33e739bf4fd642a93fb247ec8db8d5c8caf13f
Mark Linn <marktlinn@gmail.com>
2022-09-14 11:15:46 +0200

    added jsonWebToken creation on signup, saving to DB and returning with JSON doc on success

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,10 @@
 
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
+function createWebToken(_id){
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 //User login controller
 async function userLogin(req,res) {
     res.send({message: 'user login'})
@@ -8,11 +12,12 @@ async function userLogin(req,res) {
 
 //User signup controller
 async function userSignup(req,res) {
-    const {email, password } = req.body
+    const {email, password} = req.body
     try {
         const user = await User.signup(email, password)
 
-        res.status(200).json({email, user})
+        const token = createWebToken(user._id)
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
