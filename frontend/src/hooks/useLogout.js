34262df5e6aceb27c0695499310092bca1273601
Mark Linn <marktlinn@gmail.com>
2022-09-17 22:04:14 +0200

    Frontend: on each logout reset todos dispatch to null, to avoid state being retained and displayed between users

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,14 +1,16 @@
 import { useAuthContext } from './useAuthContext'
+import { useTodosContext } from './useTodosContext'
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext();
+    const { dispatch: todoDispatch } = useTodosContext();
 
     const logout = () =>{
         //remove user from local storage
         localStorage.removeItem('user')
-        //dispatch LOgOUT to reducer
+        //dispatch Logout to reducer
         dispatch({type: 'LOGOUT'})
-
+        todoDispatch({type: 'SET_TODOS', payload: null})
     }
 
     return {logout}
