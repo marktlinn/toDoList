b0a4906a1d972e23d624b8f927e2e9247a34ffda
Mark Linn <marktlinn@gmail.com>
2022-10-15 15:55:39 +0200

    fix: bug fixes with react-app- build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter >
+      <BrowserRouter basename="/">
       < Navbar 
       windowSize={windowSize}
       toggleMenu={toggleMenuOverRide}
