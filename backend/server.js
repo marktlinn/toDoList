c166450fa2140da2ce0c9803f0f816d880b646bb
Mark Linn <marktlinn@gmail.com>
2023-02-03 12:49:43 +0100

    fix: update cors on BE

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,32 @@
-require('dotenv').config( {path: './config/.env'})
-const cors = require('cors');
+require("dotenv").config({ path: "./config/.env" });
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
-const express = require('express');
-const connectDB = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
 //express app setup
-const app = express()
+const app = express();
 
 //database connection
-connectDB()
+connectDB();
 
-//middleware 
-app.use(express.json())
-app.use((req,res,next)=>{
-    console.log(req.path, req.method);
-    next();
-})
-app.use(cors())
+//middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+app.use(
+  cors({
+    origin: "https://todo-list-app-fe.onrender.com",
+  })
+);
 
 //Routes
-const todosRoutes = require('./routes/todos')
-const userRoutes = require('./routes/users')
-app.use('/api/todo',todosRoutes)
-app.use('/api/auth', userRoutes)
+const todosRoutes = require("./routes/todos");
+const userRoutes = require("./routes/users");
+app.use("/api/todo", todosRoutes);
+app.use("/api/auth", userRoutes);
 
-
-app.listen(PORT, ()=>{
-    console.log(`server running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
