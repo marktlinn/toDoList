149cb0833c82bac072b3ce94c6de6613b73e1e7f
Mark Linn <marktlinn@gmail.com>
2023-05-19 12:33:27 +0200

    chore: update github username

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import  { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div>
-            <span><small>Made by Mark Linn</small></span>
-            </div>
-            <div className='profile-tags'>
-            <a href="https://github.com/marksist300"><FontAwesomeIcon icon={faGithub} /></a>
-            <a href="https://twitter.com/marksist_300"><FontAwesomeIcon icon={faTwitter} /></a>
-            <a href="mailto:marklinndev@gmail.com"><FontAwesomeIcon icon={faEnvelope} /></a>
-            </div>
-        </footer>
-    )
+  return (
+    <footer>
+      <div>
+        <span>
+          <small>Made by Mark Linn</small>
+        </span>
+      </div>
+      <div className="profile-tags">
+        <a href="https://github.com/marktlinn">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href="https://twitter.com/marksist_300">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="mailto:marklinndev@gmail.com">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
-}
-
-export default Footer
+export default Footer;
